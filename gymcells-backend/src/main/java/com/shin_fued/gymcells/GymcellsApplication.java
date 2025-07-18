7b7d7673feb294e53dd5e06c8db0903c9f4a674b
shin-fued <shin0not0yeam@gmail.com>
2025-07-18 15:15:30 +0700

    Sync local changes before force push

---
+++ gymcells-backend/src/main/java/com/shin_fued/gymcells/GymcellsApplication.java
@@ -0,0 +1,13 @@
+package com.shin_fued.gymcells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymcellsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymcellsApplication.class, args);
+	}
+
+}
---
+++ gymcells-backend/src/test/java/com/shin_fued/gymcells/GymcellsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shin_fued.gymcells;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymcellsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
